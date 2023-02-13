8eb57c2e16462edb94bb5d65d98511276ffc2be0
SUBRAMANYA N <61598651+Subramanya-DAA@users.noreply.github.com>
2023-02-13 14:14:20 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,22 @@
+def job_selection(n, jobs):
+    jobs = sorted(jobs, key=lambda x: x[2], reverse=True)
+    earnings = 0
+    count = 0
+    end_time = 0
+    for job in jobs:
+        if job[0] >= end_time:
+            end_time = job[1]
+            earnings += job[2]
+            count += 1
+    return (n - count, earnings)
+
+n = int(input("enter no of jobs:"))
+jobs = []
+for i in range(n):
+    start_time = int(input("start time:"))
+    end_time = int(input("end time:"))
+    profit = int(input("profit:"))
+    jobs.append((start_time, end_time, profit))
+
+result = job_selection(n, jobs)
+print(result[0], result[1])
---
+++ output3.py
@@ -0,0 +1,15 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:/Users/subramanya/Desktop/a.py ==================
+enter no of jobs:3
+start time:1200
+end time:2200
+profit:200
+start time:1300
+end time:2300
+profit:500
+start time:1000
+end time:2100
+profit:300
+2 500
